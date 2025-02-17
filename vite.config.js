4b0e0a4543079a681d7dcfc4daaf644be0a73d95
JaneBee24 <bublikkjenya@gmail.com>
2025-02-17 19:17:10 +0100

    а

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,11 @@ import { defineConfig } from 'vite';
 import injectHTML from 'vite-plugin-html-inject';
 
 export default defineConfig({
+  root: 'src',  
   base: '/goit-js-hw-09/', 
   build: {
-    outDir: 'dist', 
+    outDir: '../dist', 
     emptyOutDir: true, 
   },
-  plugins: [
-    injectHTML(), 
-  ],
+  plugins: [injectHTML()],
 });
