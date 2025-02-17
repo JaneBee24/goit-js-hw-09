05579ea7f95e19f19871859a345c64663b52a9fd
JaneBee24 <bublikkjenya@gmail.com>
2025-02-17 19:09:10 +0100

    7

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,13 @@
 import { defineConfig } from 'vite';
-import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
-    root: 'src',
-    build: {
-      sourcemap: true,
-      rollupOptions: {
-        input: glob.sync('src/*.html'),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: '[name].js',
-          assetFileNames: 'assets/[name]-[hash][extname]',
-        },
-      },
-      outDir: 'dist',
-      emptyOutDir: true,
-    },
-    plugins: [
-      injectHTML(), 
-    ],
-  };
+export default defineConfig({
+  base: '/goit-js-hw-09/', 
+  build: {
+    outDir: 'dist', 
+    emptyOutDir: true, 
+  },
+  plugins: [
+    injectHTML(), 
+  ],
 });
